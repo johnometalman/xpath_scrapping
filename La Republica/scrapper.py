f108d3748a7a6df8ba2f05d387f8d6e31c314ef9
John J. Meza <johnometalmanldu@gmail.com>
2021-09-02 16:59:43 -0500

    Versión del 02 de Sep

--- La Republica/scrapper.py
+++ La Republica/scrapper.py
@@ -1,14 +1,48 @@
 import requests 
 import lxml.html as html
-
+import os 
+import datetime
 
 
 url = 'https://www.larepublica.co/'
 
-xpath_article = '//div[@class="V_Title"]/text-fill/a/@href'
-#xpath_tittle = '//div[@class="mb-auto"]/h2/span/text()'
-#xpath_summary = '//div[@class="lead"]/p/text()'
-#xpath_body = '//div[@class="html-content"]/p[not(@class)]/text()'
+xpath_article = '//h2[@class= "headline"]/a/@href'
+xpath_tittle = '//div[@class= "mb-auto"]/h2/span[1]/text()'
+xpath_summary = '//div[@class="lead"]/p/text()'
+xpath_body = '//div[@class="html-content"]/p[not(@class)]/text()'
+
+def parse_notice(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+
+            try:
+                tittle = parsed.xpath(xpath_tittle)[0]
+                tittle = tittle.replace('\"', '')
+                summary = parsed.xpath(xpath_summary)[0]
+                body = parsed.xpath(xpath_body)
+            except IndexError:
+                return
+
+            with open(f'{today}/{tittle}.txt', 'w', encoding='utf-8') as f:
+                f.write(tittle)
+                f.write('\n\n')
+                f.write(summary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+
+        else:
+            raise ValueError(f'Error:{response.status_code}')
+
+    except ValueError as ve:
+        print(ve)
+
+
+
 
 def parse_home():
     try:
@@ -17,8 +51,15 @@ def parse_home():
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links_to_notices = parsed.xpath(xpath_article)
-            print(links_to_notices) 
+            #print(links_to_notices) 
 
+            today = datetime.date.today().strftime('%d-%m-%Y')
+
+            if not os.path.isdir(today):
+                os.mkdir(today)
+
+            for link in links_to_notices:
+                parse_notice(link, today)
 
         else:
             raise ValueError(f'Error: {response.status_code}')
